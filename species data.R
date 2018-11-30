eb201ec8f410e8df46dbadba493e038076691b16
Martin Jacob <martin.jacob@ufl.edu>
2018-11-30 15:10:43 -0500

    loaded time since fire csv
    Merge branch 'master' of https://github.com/martinfjacob/LPK-Project
    
    # Conflicts:

--- species data.R
+++ species data.R
@@ -1,9 +1,9 @@
 library(dplyr)
 library(tidyr)
 
-
-veg_data <- read.csv("data/platt_veg_data_updated.csv", sep = "\t") 
 write.csv(veg_data, "new_veg_data.csv", row.names = FALSE)
+veg_data <- read.csv("data/new_veg_data.csv") 
+
 
 veg_data_by_plot <- select(veg_data, "BLOCK", "PLOT", "SPCODE", "FREQUENCY")
 
@@ -15,4 +15,6 @@ species_by_plot[c("id")] <- list(NULL)
 
 species_by_plot[is.na(species_by_plot)] <- 0
 
-time_since_fire <- read.csv("data/ENP_PlattSlocum_PlotLocationsElevationsFire.csv")
+time_since_fire <- read.csv("data/ENP_PlattSlocum_Fire.csv")
+
+
