99dcf7795216031c3e44db17fdf17c2e89c58cb7
Martin Jacob <martin.jacob@ufl.edu>
2018-11-30 13:29:28 -0500

    write.csv for data

--- species data.R
+++ species data.R
@@ -3,6 +3,7 @@ library(tidyr)
 
 
 veg_data <- read.csv("data/platt_veg_data_updated.csv", sep = "\t") 
+write.csv(veg_data, "new_veg_data.csv", row.names = FALSE)
 
 veg_data_by_plot <- select(veg_data, "BLOCK", "PLOT", "SPCODE", "FREQUENCY")
 
@@ -14,4 +15,4 @@ species_by_plot[c("id")] <- list(NULL)
 
 species_by_plot[is.na(species_by_plot)] <- 0
 
-time_since_fire <- read.csv("data/ENP_PlattSlocum_PlotLocationsElevationsFire.csv", sep = "\t")
+time_since_fire <- read.csv("data/ENP_PlattSlocum_PlotLocationsElevationsFire.csv")
